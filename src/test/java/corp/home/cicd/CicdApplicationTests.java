4f0bc06c399928595616c1126dc563f072eb1997
frs2000 <frs2000@mail.ru>
2022-07-01 17:28:36 +0300

    Excluded testing

--- src/test/java/corp/home/cicd/CicdApplicationTests.java
+++ src/test/java/corp/home/cicd/CicdApplicationTests.java
@@ -1,13 +1,13 @@
 package corp.home.cicd;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CicdApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+//
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
